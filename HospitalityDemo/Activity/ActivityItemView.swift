53bdf1bf01a1cad5e2839105caa85d3365d6602b
Robert Tucker <r.tucker@quadient.com>
2023-10-14 19:55:32 -0400

    feat: add support for deleting documents (activities) (#28)
    Fixes: #22

--- HospitalityDemo/Activity/ActivityItemView.swift
+++ HospitalityDemo/Activity/ActivityItemView.swift
@@ -8,34 +8,28 @@ import SwiftUI
 struct ActivityItemView: View {
   let activity: Activity
   
-  private let cornerRadius: CGFloat = 20
+  private let cornerRadius: CGFloat = 16
   
   var body: some View {
-    ZStack {
-      RoundedRectangle(cornerRadius: cornerRadius)
-        .strokeBorder(.clear)
-        .frame(width: 360, height: 100)
-      HStack {
-        VStack(alignment: .leading) {
-          Text(
-            "\(activity.hotelName!)",
-            comment: "Hotel name"
-          ).bold()
-          Text(
-            "Stay: \(activity.checkIn!) to \(activity.checkOut!)",
-            comment: "Dates of stay"
-          ).font(.caption)
-        }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 8)
-        Spacer()
+    HStack {
+      VStack(alignment: .leading) {
+        Text(
+          "\(activity.hotelName!)",
+          comment: "Hotel name"
+        ).bold()
+        Text(
+          "Stay: \(activity.checkIn!) to \(activity.checkOut!)",
+          comment: "Dates of stay"
+        ).font(.caption)
       }
-      .background(.white)
-      .cornerRadius(cornerRadius)
+      .padding(.horizontal, 16)
+      .padding(.vertical, 8)
+      Spacer()
     }
+    .background(Color("brand/aqua"))
+    .cornerRadius(cornerRadius)
     .padding(.horizontal, 8)
     .shadow(radius: 2)
-    .cornerRadius(cornerRadius)
   }
 }
 
