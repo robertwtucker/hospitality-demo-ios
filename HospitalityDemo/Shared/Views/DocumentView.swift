5144a6941b06fe1d8e8cb22a97f44b635c86ac6c
Robert Tucker <r.tucker@quadient.com>
2023-10-17 00:46:48 -0400

    feat: add progress view overlays while DAS documents load
    Closes: #34

--- HospitalityDemo/Shared/Views/DocumentView.swift
+++ HospitalityDemo/Shared/Views/DocumentView.swift
@@ -7,21 +7,72 @@ import SwiftUI
 
 struct DocumentView: View {
   var document: DocumentInfo
+  @SwiftUI.State var viewState: AdvantageSdkDocumentViewState = .loading
   
   var body: some View {
-    AdvantageSdkDocumentView(document: document)
+    ZStack {
+      AdvantageSdkDocumentView(document: document, viewState: $viewState)
+      viewStateOverlay
+    }
+  }
+  
+  @ViewBuilder
+  private var viewStateOverlay: some View {
+    switch viewState {
+    case .loading:
+      LoadingView()
+    case .error(let message):
+      ErrorView(error: message)
+    default:
+      EmptyView()
+    }
   }
 }
+
+// MARK: - AdvantageSdkDocumentViewState
+
+enum AdvantageSdkDocumentViewState {
+  case loading
+  case loaded
+  case error(String)
+}
+
+// MARK: - AdvantageSdkDocumentViewStateTracking
+
+protocol AdvantageSdkDocumentViewStateTracking {
+  var documentViewState: AdvantageSdkDocumentViewState { get set }
+}
+
+// MARK: - AdvantageSdkDocumentView
 
 struct AdvantageSdkDocumentView: UIViewControllerRepresentable {
   var document: DocumentInfo
+  @Binding var viewState: AdvantageSdkDocumentViewState
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(self)
+  }
   
   func makeUIViewController(context: Context) -> DCViewController {
-    return DCViewController()
+    let dcViewController = DCViewController()
+    dcViewController.document = document
+    dcViewController.documentViewStateDelegate = context.coordinator
+    return dcViewController
   }
   
-  func updateUIViewController(_ controller: DCViewController, context: Context) {
-    controller.document = document
+  func updateUIViewController(_ controller: DCViewController, context: Context) { }
+  
+  class Coordinator: NSObject, AdvantageSdkDocumentViewStateTracking {
+    var parent: AdvantageSdkDocumentView
+    
+    init(_ advantageSdkDocumentView: AdvantageSdkDocumentView) {
+      self.parent = advantageSdkDocumentView
+    }
+    
+    var documentViewState: AdvantageSdkDocumentViewState = .loading {
+      didSet {
+        parent.viewState = documentViewState
+      }
+    }
   }
 }
-
