ccfba71cfafcef486d66a5015beb9a9e2970d9df
Jay Stobie <144940258+jstobie-quadient@users.noreply.github.com>
2023-10-26 13:55:25 -0500

    feat: add new checkout screens (#48)
    * feat: included new views for check out process
    * feat: checkout features added
    ---------
    
    Co-authored-by: Robert Tucker <r.tucker@quadient.com>

--- HospitalityDemo/CheckOut/CheckOutView.swift
+++ HospitalityDemo/CheckOut/CheckOutView.swift
@@ -12,42 +12,86 @@ struct CheckOutView: View {
   @SwiftUI.State private var isProcessing = false
   @SwiftUI.State private var sendEmail = false
   @SwiftUI.State private var showConfirm = false
+  @SwiftUI.State private var additionalInformation = ""
+  let maxCharacters = 255
   
   private let logger = Logger(
     subsystem: Bundle.main.bundleIdentifier!,
     category: String(describing: CheckOutView.self))
   
   var body: some View {
-    VStack(spacing: 16) {
-      Spacer()
-      Text("checkout.temp.viewname")
-        .font(.title)
-      Toggle("checkout.sendEmail", isOn: $sendEmail)
-        .padding(.horizontal, 32)
-        .padding(.vertical, 24)
-      Button {
-        Task {
-          isProcessing.toggle()
-          stay.currentStay?.sendEmail = sendEmail
-          await stay.checkOut()
-          isProcessing.toggle()
-          showConfirm.toggle()
-        }
-      } label: {
+    VStack {
+      VStack {
+        Image("general/checkOut")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(maxWidth: .infinity)
         HStack {
+          Image("hotel/logo")
+            .padding(.leading, 20)
+            .frame(height: 25)
+            .aspectRatio(contentMode: .fit)
+          Text("\(stay.currentStay?.reservation.hotel.name ?? "Hotel Name")")
+            .font(.caption)
+            .padding(.vertical, 10)
+            .bold()
           Spacer()
-          if isProcessing {
-            ProgressView()
-          } else {
-            Text("button.checkout")
-          }
-          Spacer()
+          RatingView(rating: stay.currentStay?.reservation.hotel.rating ?? 0.0).padding(.horizontal, 20)
         }
+        .background(Color(UIColor.systemBackground))
+        .shadow(radius: 10, x: 0, y: 0)
       }
-      Spacer()
-    }
-    .sheet(isPresented: $showConfirm) {
-      CheckOutConfirmView()
+      
+      VStack(spacing: 32) {
+        Text("checkout.temp.viewname")
+          .font(.title)
+          .padding(.top, 32)
+          .foregroundColor(Color("brand/brown"))
+        VStack(alignment: .leading) {
+          Text("Would you like to leave any feedback?")
+            .font(.footnote)
+          TextEditor(text: $additionalInformation)
+            .foregroundColor(.gray)
+            .frame(height: 100)
+            .border(Color("brand/brown"))
+            .font(.caption)
+          Text("\(maxCharacters-additionalInformation.count)")
+            .font(.caption)
+            .foregroundColor(additionalInformation.count <= maxCharacters ? .gray : .red)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        
+        Toggle("checkout.sendEmail", isOn: $sendEmail)
+        Button {
+          Task {
+            isProcessing.toggle()
+            stay.currentStay?.sendEmail = sendEmail
+            await stay.checkOut()
+            isProcessing.toggle()
+            showConfirm.toggle()
+          }
+        } label: {
+          HStack {
+            Spacer()
+            if isProcessing {
+              ProgressView()
+            } else {
+              Text("button.checkout")
+            }
+            Spacer()
+          }
+        }
+        .background(Color("brand/turquoise"))
+        .foregroundColor(.white)
+        .buttonStyle(.bordered)
+        .controlSize(.large)
+        .cornerRadius(10)
+        Spacer()
+      }
+      .padding(.horizontal, 20)
+      .sheet(isPresented: $showConfirm) {
+        CheckOutConfirmView()
+      }
     }
   }
 }
